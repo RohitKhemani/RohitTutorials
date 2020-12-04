16f785e4a6d57ee4489d7ea0c5bd9374abe8db78
Rohit <rohitkhemani2@gmail.com>
2020-12-04 15:35:46 +0530

    FIRST PUSH

--- src/Advance/Date.java
+++ src/Advance/Date.java
@@ -3,29 +3,29 @@ package Advance;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Google {
+public class Date {
 	
 	public static WebDriver driver;
 
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws InterruptedException {
 
 		System.setProperty("webdriver.chrome.driver","F:\\Selenium\\Zip Files\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		
 		driver.get("https://jqueryui.com/datepicker/");
-
 		
-		driver.findElement(By.id("datepicker")).click();
+Thread.sleep(3000);
 		
+driver.findElement(By.xpath("//input[@id='datepicker']")).click();		
 		
 		
 	}
+
+
 
 }
